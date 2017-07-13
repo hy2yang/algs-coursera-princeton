cb5938c19fdc1798cf8fe9a13557f85c5bec39b7
hy2yang <frostsobi@live.com>
2017-07-13 10:57:59 +0800

    algs4 files first commit

---
+++ algs4/Percolation.java
@@ -0,0 +1,108 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+	public static void main(String[] args)  // TODO Auto-generated method stub
+	{
+		
+	}
+	
+	
+	private int[] grid;
+	private int opensites;
+	private int s;
+	private WeightedQuickUnionUF a;
+	
+	public Percolation(int n)   // create n-by-n grid, with all sites blocked
+	{  
+		if (n<1) {
+            throw new IllegalArgumentException("the grid can not be smaller than 1x1");  
+        }
+		s=n;
+		a=new WeightedQuickUnionUF(s*s+2);  // s*s+2 sites, index from 0 to s*s+1
+		opensites=0;
+		grid= new int[s*s+2];  // 0=top dummy, s*s+1=bottom dummy, grid index 1~s*s
+		
+	}
+	
+	private void validate(int p) {
+        if (p <= 0 || p > s) {
+            throw new IllegalArgumentException("index " + p + " is not between 1 and " + s);  
+        }
+    }
+	
+	public boolean isOpen(int row, int col)
+	{
+		validate(row);
+		validate(col);
+		int i=(row-1)*s+col;
+		return (grid[i]==1);
+	}
+	
+	public boolean isFull(int row, int col)
+	{
+		validate(row);
+		validate(col);
+		int i=(row-1)*s+col;
+		return (a.find(i)==0);
+	}
+	
+	public int numberOfOpenSites()
+	{
+		return opensites;
+	}
+	
+	public  void open(int row, int col)
+	{
+		validate(row);
+		validate(col);
+		int i=(row-1)*s+col-1;
+		
+		if (grid[i]==0)
+		{
+			grid[i]=1;
+			opensites+=1;
+			
+			if (i>0&i<=s) //top row
+			{
+				a.union(i,0);
+				
+			}
+			
+			if (i>s*s-s) //bottom row
+			{
+				a.union(i,s*s+1);
+			}
+			
+			if((i%s!=1) & grid[i-1]==1)  //not left col
+			{
+				a.union(i,i-1);
+			}
+			
+			if((i%s!=0) & grid[i+1]==1) //not right col
+			{
+				a.union(i,i+1);
+			}
+			
+			if(i>s & grid[i-s]==1)  //not top
+			{
+				a.union(i,i-s);
+			}
+			
+			if((i<=s*s-s) & grid[i+s]==1)  //not bottom
+			{
+				a.union(i,i+s);
+			}
+		}
+		
+		
+	}
+	
+	public boolean percolates()
+	{
+	 return (a.connected(0, s*s+1));
+	}
+	
+}
+
+
