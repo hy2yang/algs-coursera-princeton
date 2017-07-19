a83a4a758fbcee04649d6af2d90a9ff3ffe46a8b
hy2yang <Frost@yang-pc>
2017-07-19 17:50:56 +0800

    debug

--- algs4/Deque.java
+++ algs4/Deque.java
@@ -2,55 +2,52 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> {
-	
-	private class node
-	{
+
+	private class Node{
 		Item item;
-		node previous, next;
+		Node previous, next;
 	}
 	
-	private node first, last;
+	private Node first, last;
 	private int dequesize;
 	
-	public boolean isEmpty()
-	{
+	public boolean isEmpty(){
 		return (dequesize==0);
 	}
 	
-	public int size()
-	{
+	public int size(){
 		return dequesize;
 	}
 	
-	public Deque()
-	{
+	public Deque(){
 		dequesize = 0;
-		first = null;
-		last = null; 
+		last = first = new Node();
+		
 	}
 	
-	public void addFirst(Item item)
-	{
+	public void addFirst(Item item){
 		if (item == null) throw new IllegalArgumentException("can not add null item");
-		node oldfirst = first;
-		first = new node();
+		Node oldfirst = first;
+		first = new Node();
 		first.item = item;
         first.next = oldfirst;
+        oldfirst.previous=first;
         dequesize++;
+        if (dequesize == 1) last = first;        
 	}
 
-	public void addLast(Item item)
-	{
+	public void addLast(Item item){
 		if (item == null) throw new IllegalArgumentException("can not add a null item");
-		node oldlast = last;
-		last = new node();
+		Node oldlast = last;
+		last = new Node();
 		last.item = item;
-        first.previous = oldlast;
+        last.previous = oldlast;
+        oldlast.next=last;
         dequesize++;
+        if (dequesize == 1) first = last;        
 	}
 	
-	public Item removeFirst()
-	{
+	public Item removeFirst(){
 		if (isEmpty()) throw new NoSuchElementException("Stack underflow");
 		Item item = first.item;        
         first = first.next;           
@@ -58,8 +55,7 @@ public class Deque<Item> {
         return item;
 	}
 	
-	public Item removeLast()
-	{
+	public Item removeLast(){
 		if (isEmpty()) throw new NoSuchElementException("Stack underflow");
 		Item item = last.item;        
         last = last.previous;           
@@ -72,8 +68,8 @@ public class Deque<Item> {
 		return new ListIterator();
 	}
 	
-	private class ListIterator implements Iterator<Item> {
-        private node current = first;
+	private class ListIterator implements Iterator<Item>{
+        private Node current = first;
         public boolean hasNext()  { return current != null;                     }
         public void remove()      { throw new UnsupportedOperationException();  }
 
@@ -87,6 +83,16 @@ public class Deque<Item> {
 	
 	public static void main(String[] args)
 	{
+	/*	Deque<Integer> s= new Deque<Integer>();		
+		for(int i=0; i<21;i++)
+		{
+			s.addLast(i);
+		}
+		
+		for(int j=0; j<21;j++)
+		{
+			StdOut.print(s.removeLast()+" ");
+		} */
 		
 	}
 
