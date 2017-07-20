70bbfc27bb6bd3251a17c45597e6691172d973c2
hy2yang <Frost@yang-pc>
2017-07-20 21:13:35 +0800

    81/100

---
+++ algs4/Permutation.java
@@ -0,0 +1,17 @@
+
+public class Permutation {
+
+    public static void main(String[] args) {      
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+        
+        while (!StdIn.isEmpty()) {            
+            q.enqueue(StdIn.readString());
+        }
+        
+        for (int i=0;i<k;i++){
+            StdOut.println(q.dequeue());            
+        }
+    }
+
+}
